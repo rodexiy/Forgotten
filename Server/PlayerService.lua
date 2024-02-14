1f5325187cbe19491384eaf43ae566720e490dc7
rodexiy <rodexcontato@gmail.com>
2024-02-14 20:40:11 -0300

    PlayerService Update

--- Server/PlayerService.lua
+++ Server/PlayerService.lua
@@ -5,6 +5,7 @@ function PlayerService.PlayerAdded(Player)
     print(Player.Name, " Joined the game!")
 end
 
+
 function PlayerService.Init()
     game.Players.PlayerAdded:Connect(PlayerService.PlayerAdded)
 end
